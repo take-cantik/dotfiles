a758b6dc1b577a5c89de7323c7bd70d5d359ff3c
take-cantik <take.cantik17@gmail.com>
2022-01-23 20:54:54 +0900

    Add: git modules

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .gitconfig .gitconfig.user .vimrc .vim/ .zsh/)
+dotfiles=(.zshrc .gitconfig .gitconfig.user .gitmodules .vimrc .vim/ .zsh/)
 
 # 設定ファイルのシンボリックリンクをホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
