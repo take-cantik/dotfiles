b05af4ecc7688c88462a8bfa516a70097f6962ef
take-cantik <take.cantik17@gmail.com>
2022-01-23 20:25:03 +0900

    Add: .vim

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .gitconfig .gitconfig.user .vimrc)
+dotfiles=(.zshrc .gitconfig .gitconfig.user .vimrc .vim/)
 
 # 設定ファイルのシンボリックリンクをホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
