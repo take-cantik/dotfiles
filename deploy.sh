5dcffe4abebd233d00ee95d6f2a9ed6dcd57697e
take-cantik <take.cantik17@gmail.com>
2023-10-25 17:02:04 +0900

    :zap: update path

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ dotfiles=(.zshrc .gitconfig .gitconfig.user .gitmodules .vimrc .vim/ .zsh/)
 
 # 設定ファイルのシンボリックリンクをホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-  ln -svf ~/works/dotfiles/$file ~
+  ln -svf ${PWD}/"$file" ~
 done
 
