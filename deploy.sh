f5600a67d6c8d1efed012b61eb506c04bb64918f
take-cantik <take.cantik17@gmail.com>
2023-12-28 03:33:10 +0900

    :zap: Add files

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .gitconfig .gitconfig.user .gitmodules .vimrc .yabairc .skhdrc .vim/ .zsh/)
+dotfiles=(.zshrc .gitconfig .gitconfig.user .gitmodules .vimrc .yabairc .skhdrc .vim/ .zsh/ '.config/nvim/lua')
 
 # 設定ファイルのシンボリックリンクをホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-  ln -svf ${PWD}/"$file" ~
+  ln -svf ${PWD}/"$file" ~/${file}
 done
 
