5de018d967ded6cc8c484d4f8b0b77a0d85f3290
take-cantik <take.cantik17@gmail.com>
2024-01-07 02:03:53 +0900

    :wrench: Add base setting

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -4,3 +4,8 @@ vim.scriptencoding = "utf-8"
 
 vim.wo.number = true
 
+-- Open ahi file
+vim.api.nvim_create_user_command("Ahi", function(opts)
+  vim.cmd("e " .. "~/_/ahi/ahi.markdown")
+end, {})
+
