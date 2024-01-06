72fae72af638293b208423aa419751397d4b9c23
take-cantik <take.cantik17@gmail.com>
2024-01-07 02:05:27 +0900

    :zap: Add keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,5 @@
 local opts = { noremap = true, silent = true }
+local expr_opts = { expr = true, noremap = true, silent = true }
 local term_opts = { silent = true }
 
 --local keymap = vim.keymap
@@ -10,12 +11,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Modes
---   normal_mode = 'n',
---   insert_mode = 'i',
---   visual_mode = 'v',
---   visual_block_mode = 'x',
---   term_mode = 't',
---   command_mode = 'c',
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 
 -- Normal --
 -- Better window navigation
@@ -40,10 +41,7 @@ keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
 keymap("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Do not yank with x
-keymap("n", "x", '"_x', opts)
-
--- Delete a word backwards
-keymap("n", "dw", 'vb"_d', opts)
+keymap("n", "x", "_x", opts)
 
 -- 行の端に行く
 keymap("n", "<Space>h", "^", opts)
@@ -61,9 +59,8 @@ keymap("n", "<Space>q", ":<C-u>q!<Return>", opts)
 -- ESC*2 でハイライトやめる
 keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
 
--- Insert --
--- Press jk fast to exit insert mode
-keymap("i", "jk", "<ESC>", opts)
+-- jj でESC を行う
+keymap("i", "jj", "<Esc>", opts)
 
 -- コンマの後に自動的にスペースを挿入
 keymap("i", ",", ",<Space>", opts)
@@ -76,6 +73,58 @@ keymap("v", ">", ">gv", opts)
 -- ビジュアルモード時vで行末まで選択
 keymap("v", "v", "$h", opts)
 
--- 0番レジスタを使いやすくした
-keymap("v", "<C-p>", '"0p', opts)
+-- Fern.nvimの設定
+keymap("n", "<C-b>", ":Fern . -reveal=% -drawer -toggle -width=40<CR>", opts)
+
+-- Barbar.nvimの設定
+
+-- Move to previous/next
+keymap("n", "<Space>,", ":BufferPrevious<CR>", opts)
+keymap("n", "<Space>.", ":BufferNext<CR>", opts)
+
+-- Re-order to previous/next
+keymap("n", "<Space><", ":BufferMovePrevious<CR>", opts)
+keymap("n", "<Space>>", ":BufferMoveNext<CR>", opts)
+
+-- Goto buffer in position...
+keymap("n", "<Space>1", ":BufferGoto 1<CR>", opts)
+keymap("n", "<Space>2", ":BufferGoto 2<CR>", opts)
+keymap("n", "<Space>3", ":BufferGoto 3<CR>", opts)
+keymap("n", "<Space>4", ":BufferGoto 4<CR>", opts)
+keymap("n", "<Space>5", ":BufferGoto 5<CR>", opts)
+keymap("n", "<Space>6", ":BufferGoto 6<CR>", opts)
+keymap("n", "<Space>7", ":BufferGoto 7<CR>", opts)
+keymap("n", "<Space>8", ":BufferGoto 8<CR>", opts)
+keymap("n", "<Space>9", ":BufferGoto 9<CR>", opts)
+keymap("n", "<Space>0", ":BufferLast<CR>", opts)
+
+-- Pin/unpin buffer
+keymap("n", "<Space>p", ":BufferPin<CR>", opts)
+
+-- Close buffer
+keymap("n", "<Space>c", ":BufferClose<CR>", opts)
+
+-- telescope.nvim
+keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
+keymap("n", "<C-f>", ":Telescope live_grep<CR>", opts)
+
+-- floaterm.nvim
+keymap("n", "<C-t>", ":ToggleTerm<CR>", opts)
+keymap("t", "<C-t>", "<C-\\><C-n>:ToggleTerm<CR>", opts)
+
+-- GitDiff
+keymap("n", ":GitDi", ":DiffviewOpen", opts)
+
+-- Coc.nvim
+keymap("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<CR>'", expr_opts)
+keymap("i", "<C-j>", "coc#pum#visible() ? coc#pum#next() : '<C-j>'", expr_opts)
+keymap("i", "<C-k>", "coc#pum#visible() ? coc#pum#prev() : '<C-k>'", expr_opts)
+
+-- LSP
+-- keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+-- keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+
+-- AnyJump
+keymap("n", "gd", "<cmd>AnyJump<CR>", opts)
+keymap("n", "gi", "<cmd>AnyJumpBack<CR>", opts)
 
