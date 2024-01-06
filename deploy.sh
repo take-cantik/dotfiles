2cb9c8de6d2ac2e31f346c20a39829aa060e312b
take-cantik <take.cantik17@gmail.com>
2024-01-07 02:02:50 +0900

    :zap: .config用とフォルダ用で分けた

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .gitconfig .gitconfig.user .gitmodules .vimrc .yabairc .skhdrc .vim/ .zsh/ '.config/nvim/lua')
+dotfiles=(.zshrc .gitconfig .gitconfig.user .gitmodules .vimrc .yabairc .skhdrc)
 
 # 設定ファイルのシンボリックリンクをホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-  ln -svf ${PWD}/"$file" ~/${file}
+  ln -svf ${PWD}/"$file" ~
+done
+
+# 配置したい設定ファイル
+dotfolders=(.vim/ .zsh/)
+
+# 設定フォルダのシンボリックリンクをホームディレクトリ直下に作成する
+for folder in "${dotfolders[@]}"; do
+  ln -svf ${PWD}/"$folder" ~
+done
+
+#.config以下の設定ファイルを配置する
+configfolders=(nvim/ gh/)
+
+for folder in "${configfolders[@]}"; do
+  sudo ln -svf ${PWD}/.config/"$folder" ~/.config
 done
 
